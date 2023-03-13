d9af8601700216016f77763c6d34659da64e0745
bt2561 <bt2561@columbia.edu>
2023-03-13 10:01:08 +0100

    Update main.js

--- exercise7/main.js
+++ exercise7/main.js
@@ -6,3 +6,27 @@ function sayHello() {
 
 sayHello ()
 
+const age = 27;
+const height = 4.5;
+
+if(age < 8){
+  console.log("Check out the merry go round!")
+} else if (age > 8 && age < 65 && height > 4.5){
+  console.log("Check out the roller coaster!")
+} else {
+  console.log("Check out the Lazy River!")
+}
+
+let noun = ['heart', 'feet', 'stomach', 'eyes', 'hands'];
+let adjective = ['crazy', 'excitedly', 'quickly', 'once', 'never'];
+let verb = ['look', 'listen', 'hear', 'touch', 'smell'];
+
+let randomNoun = noun[Math.floor(Math.random()*noun.length)];
+let randomAdjective = adjective[Math.floor(Math.random()*adjective.length)];
+let randomVerb = verb[Math.floor(Math.random()*verb.length)];
+
+console.log(`My ${randomNoun} leaps ${randomAdjective} when I ${randomVerb} at a rainbow in the sky`)
+
+
+
+  
