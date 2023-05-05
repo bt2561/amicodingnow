a037349cf119cccee7135dd981c5454a78810da8
bt2561 <bt2561@columbia.edu>
2023-05-05 17:21:05 -0400

    edit

---
+++ exercise8/main.js
@@ -0,0 +1,35 @@
+
+console.log('this works')
+
+function sayHello() {
+    console.log('Hi')
+}
+
+sayHello ()
+
+let grayButton = document.getElementById("grayButton");
+let blueButton = document.getElementById("blueButton");
+let yellowButton = document.getElementById("yellowButton");
+
+
+function switchToGray() {
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "gray";
+  body.style.color = "white";
+}
+
+function switchToBlue() {
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "blue";
+  body.style.color = "white";
+}
+
+function switchToYellow() {
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "yellow";
+  body.style.color = "black";
+}
+
+grayButton.addEventListener("click", switchToGray);
+blueButton.addEventListener("click", switchToBlue);
+yellowButton.addEventListener("click", switchToYellow);
